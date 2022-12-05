9fda5729a1b5d75d32e48b77206977a2bb7423a6
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2022-12-05 11:41:50 -0300

    1.0.3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,19 +51,45 @@ export default class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="icon" href="/box.png" />
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://sistock.vercel.app/" />
+          <meta property="og:url" content="https://sistock.com.br/" />
           <meta property="og:title" content="Sistock" />
           <meta
             property="og:description"
             content="Sistema de gerenciamento de estoque"
           />
-          <meta
-            property="og:image"
-            content="https://sistock.vercel.app/box.png"
-          />
+          <meta property="og:image" content="https://sistock.com.br/box.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap"
+            rel="stylesheet"
+          />
+
+          <meta name="application-name" content="Sistock" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Sistock" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#20aaff" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#fafafa" />
+
+          <link rel="apple-touch-icon" href="https://sistock.com.br/box.png" />
+          <link rel="shortcut icon" href="https://sistock.com.br/box.png" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://sistock.com.br/box.png" />
+          <meta name="twitter:title" content="Sistock" />
+          <meta
+            name="twitter:description"
+            content="Sitema de gerenciamento de estoque"
+          />
+          <meta name="twitter:image" content="https://sistock.com.br/box.png" />
+          <meta name="twitter:creator" content="@rotomdapestebr" />
         </Head>
         <body>
           <Main />
